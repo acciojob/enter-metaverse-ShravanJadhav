15b4d927f5dd044a40f0df7723ecaf58cfa3ad69
Shravan Jadhav <96675283+ShravanJadhav@users.noreply.github.com>
2023-06-23 18:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const enterBtn = document.getElementById("enterBtn");
+const status = document.getElementById("status");
+
+enterBtn.addEventListener("click", function() {
+  status.innerHTML = "<h1>Entered Metaverse</h1>";
+});
